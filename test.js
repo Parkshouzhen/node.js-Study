8a197e81f6d3316abf25e53e829d0dcb9eab0761
Parkshouzhen <shouzhenn@gmail.com>
2021-01-03 21:49:07 +0900

    test commit

--- test.js
+++ test.js
@@ -1,15 +1,1 @@
-const nodemailer = require('nodemailer')
-const email = {
-    "host":"",
-    "port":"",
-    "secure":"",
-    "auth":{
-        "user":"",
-        "pass":""
-    }
-
-};
-
-const send = async(ontion)=>{
-    nodemailer.createTransport(email)
-} 
+console.log('hello')
